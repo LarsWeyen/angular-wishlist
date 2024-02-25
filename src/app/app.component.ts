87a8bff579eb717abde8d3ff50f021c305cc1841
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-02-25 15:36:04 +0100

    feat: add default wishes and component imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { WishItem } from '../shared/models/wishItem';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { WishListComponent } from './wish-list/wish-list.component';
+import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
+import { WishFilterComponent } from './wish-filter/wish-filter.component';
+import { WishListItemComponent } from './wish-list-item/wish-list-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    WishListComponent,
+    AddWishFormComponent,
+    WishFilterComponent,
+    WishListItemComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-wishlist';
+  items: WishItem[] = [
+    new WishItem('Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itself'),
+  ];
+
+  filter: any;
 }
