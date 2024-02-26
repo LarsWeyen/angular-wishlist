7b3b1d791cc7fc41a1d9d71c671f729aa372df49
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-02-26 12:06:33 +0100

    feat: add contact form to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WishModule } from './wish/wish.module';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    WishModule
+    WishModule,
+    ContactComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
